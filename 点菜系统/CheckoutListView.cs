f9f88b50be074fe48f1d4d10ff278c3a47ce5c10
SinLie <1937652268@qq.com>
2025-06-07 00:24:55 +0800

    仓库初始化，将点菜系统（ending）提交到本地仓库

---
+++ 点菜系统/CheckoutListView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 点菜系统
+{
+    public partial class CheckoutListView : ListView
+    {
+        //构造函数
+        public CheckoutListView(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public void show()
+        {
+
+        }
+
+    }
+}
---
+++ 点菜系统/控件_Combobox_按菜名查询.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace 点菜系统
+{
+    public partial class ComboboxSearchByName : UserControl
+    {
+        public ComboboxSearchByName()
+        {
+            InitializeComponent();
+        }
+
+        //菜名
+        public string dishName="全部";
+
+        //加载Combobox的Items
+        private void ComboboxSearchByName_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("全部");
+            comboBox1.Text = "全部";
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("菜品-蔬菜类.xml");
+            XmlNodeList nodeList = xmlDoc.SelectNodes("//Dish");
+            foreach(XmlNode xn in nodeList)
+            {
+                comboBox1.Items.Add(xn.ChildNodes[0].InnerText);
+            }
+
+            
+            xmlDoc.Load("菜品-荤菜类.xml");
+            nodeList = xmlDoc.SelectNodes("//Dish");
+            foreach (XmlNode xn in nodeList)
+            {
+                comboBox1.Items.Add(xn.ChildNodes[0].InnerText);
+            }
+
+            xmlDoc.Load("菜品-汤类.xml");
+            nodeList = xmlDoc.SelectNodes("//Dish");
+            foreach (XmlNode xn in nodeList)
+            {
+                comboBox1.Items.Add(xn.ChildNodes[0].InnerText);
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dishName = comboBox1.Text;
+        }
+
+
+    }
+}
